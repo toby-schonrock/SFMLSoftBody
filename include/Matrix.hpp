b6eb51b44f941a1c00b49536aca57f5faed0c488
toby-schonrock <tobias@schonrocks.com>
2022-01-22 01:14:03 +0000

    Bounce Jiggle

--- include/Matrix.hpp
+++ include/Matrix.hpp
@@ -10,12 +10,22 @@ public:
 
     Matrix(int x_, int y_) : v(x_ * y_, T{}), sizeX(x_), sizeY(y_) {}
 
-    T& operator()(int x_,int y_)  {
+    T& operator()(int x_, int y_)  {
         return v[x_ + y_ * sizeX];
     }
 
-    const T& operator()(int x_,int y_) const {
+    const T& operator()(int x_, int y_) const {
         return v[x_ + y_ * sizeX];
+    }
+
+    template <typename Callback>
+    void forEach(const Callback& cb) {
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                T& p = this-> operator()(x, y);
+                cb(p);
+            }
+        }
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -31,16 +31,26 @@ public:
         shape.setOrigin(visualize(Vec2(radius, radius)));
     }
 
-    void update(double deltaTime, sf::RenderWindow& window, double gravity) {
+    void draw(sf::RenderWindow& window) {
+            shape.setPosition(visualize(pos));
+            window.draw(shape);
+    }
+
+    void update(double deltaTime, double gravity) {
         f += Vec2(0, 1) * (gravity * mass); // add gravity to the force
         vel += (f * deltaTime) / mass; // euler integration could be improved
         pos += vel * deltaTime;
-        shape.setPosition(visualize(pos));
-        window.draw(shape);
         // std::cout << deltaTime << '\n';
         // std::cout << pos << '\n';
         f = Vec2();
     }
+
+    void polyColHandler() {
+            if (pos.y > 7) {
+                vel.y *= -1;
+                pos.y = 7;
+            }
+        }
 };
 
 sf::Vector2f visualize(const Vec2& v) {
@@ -48,8 +58,8 @@ sf::Vector2f visualize(const Vec2& v) {
 }
 
 void springHandler(Point& p1, Point& p2, double stableScale) {
-    static constexpr double stablePoint = 0.22;
-    static constexpr double springConst = 14;
+    static constexpr double stablePoint = 0.2;
+    static constexpr double springConst = 300;
     static constexpr double dampFact = 2;
 
     Vec2 diff = p1.pos - p2.pos;
@@ -80,26 +90,27 @@ void simFrame(Matrix<Point>& points) {
     }
 }
 
-void displayFps(double fps, sf::RenderWindow& window){
-    sf::Font font;
-    if (!font.loadFromFile("arial.ttf")) {
-        // error...
-    }
+void displayFps(double Vfps, double Sfps, sf::RenderWindow& window, sf::Font font){
     sf::Text text;
     text.setFont(font); // font is a sf::Font
-    text.setString(std::to_string(fps));
+    text.setString(std::to_string(Vfps) + " " + std::to_string(Sfps));
     text.setCharacterSize(24); // in pixels, not points!
     text.setFillColor(sf::Color::Red);
     window.draw(text);
 }
 
 int main() {
-    constexpr Vec2 scale(4, 4);
+    constexpr Vec2 scale(3, 3);
     constexpr Vec2 gap(0.2, 0.2);
     constexpr Vec2I size(static_cast<int>(scale.x / gap.x), static_cast<int>(scale.y / gap.y));
     constexpr Vec2 simPos(3, 3);
     constexpr double radius = 0.05;
-    constexpr double gravity = 0;//9.8;
+    constexpr double gravity = 0.4;
+
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+        // error...
+    }
 
     std::cout << Vec2(0,1) - Vec2(0,2) << '\n'; // [0, 1]
     std::cout << (Vec2(0,0) - Vec2(1,1)).mag() << '\n'; // 1.41421
@@ -118,7 +129,6 @@ int main() {
     // 1.. is one meter
  
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Soft Body Simulation"); //, sf::Style::Fullscreen);
-    window.setFramerateLimit(100);
 
     Matrix<Point> points(size.x, size.y);
     for (int x = 0; x < size.x; x++) {
@@ -130,7 +140,7 @@ int main() {
     // Point p(Vec2(0, 0), radius, 1.0);
 
     std::chrono::_V2::system_clock::time_point last = std::chrono::high_resolution_clock::now();
-    double fps = 0;
+    double Vfps = 0;
 
     while (window.isOpen()) {
         std::chrono::_V2::system_clock::time_point start = std::chrono::high_resolution_clock::now();
@@ -140,30 +150,36 @@ int main() {
         while (window.pollEvent(event)) {
              if (event.type == sf::Event::Closed) window.close();
         }
-        window.clear();
-
-        displayFps(fps, window);
-
-        std::chrono::nanoseconds deltatime = std::chrono::high_resolution_clock::now() - last;
-        last = std::chrono::high_resolution_clock::now();
-
-        // std::cout << nanos.count() << '\n';
-        simFrame(points);
-        for (int x = 0; x < points.sizeX; x++) {
-            for (int y = 0; y < points.sizeY; y++) {
-                points(x, y).update(static_cast<double>(deltatime.count()) / 1e9, window, gravity);
-            }
+        
+        std::chrono::nanoseconds sinceVFrame = std::chrono::high_resolution_clock::now() - start;
+        while ((sinceVFrame.count() < 10'000'000)) { // TODO: min max avg frames test
+            std::chrono::_V2::system_clock::time_point newLast = std::chrono::high_resolution_clock::now();
+            std::chrono::nanoseconds deltaTime = newLast - last;
+            // std::cout << sinceVFrame.count() << '\n';
+            simFrame(points);
+            points.forEach([deltaTime] (Point& p) {
+                p.update(static_cast<double>(deltaTime.count()) / 1e9, gravity);
+                p.polyColHandler();
+            });
+            last = newLast;
+            sinceVFrame = std::chrono::high_resolution_clock::now() - start;
         }
-        // p.update(static_cast<double>(nanos.count()) / 1000000.0, window, gravity);
+
+        // draw
+        std::chrono::nanoseconds deltaTime = std::chrono::high_resolution_clock::now() - last;  // TODO: avg
+        window.clear();       
+        double Sfps = 1e9 / static_cast<double>(deltaTime.count());
+        displayFps(Vfps, Sfps, window, font);
+
+        points.forEach([&window] (Point& p) {
+            p.draw(window);
+        });
 
         window.display();
-        std::chrono::nanoseconds nanos = std::chrono::high_resolution_clock::now() - start;
-        std::cout << nanos.count();
-        while ((nanos.count() < 1'000'000)) {
-            nanos = std::chrono::high_resolution_clock::now() - start;
-        }
-        std::cout << " - " << nanos.count() << "ns\n";
-        fps = 1e9 / static_cast<double>(nanos.count());
+      
+        sinceVFrame = std::chrono::high_resolution_clock::now() - start;
+        std::cout << sinceVFrame.count() << "ns\n";
+        Vfps = 1e9 / static_cast<double>(sinceVFrame.count());
     }
 
     return 0;
